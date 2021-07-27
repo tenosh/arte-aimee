f7a27ef6b029bfcb6b4bcf2a015394aabe7fd903
Diego <hd-diegocortez@hristovdevelopment.com>
2021-07-27 18:06:16 -0500

    Commit message

---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  let x = 0
+	let y = 0
+	let diego = `brah`;
+
+	function yPlusAValue(value) {
+		return value + y
+	}
+	$: total = yPlusAValue(x)
+</script>
+<main>
+  Total: {total}
+<button on:click={() => x++}>
+	Increment X
+</button>
+
+<button on:click={() => y++}>
+	Increment Y
+</button>
+</main>
+<style global lang="postcss">
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
